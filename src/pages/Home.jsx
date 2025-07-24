994fdc207d89ecec008d539af2d002bacdbd19b5
Missab <111555122+missab10@users.noreply.github.com>
2025-07-24 13:55:15 +0530

    whatsapp1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -575,6 +575,7 @@ const HeroSection = () => {
               >
                 <option value="">Select Gender</option>
                 <option value="Male">Male</option>
+                
                 <option value="Female">Female</option>
               </select>
             </div>
